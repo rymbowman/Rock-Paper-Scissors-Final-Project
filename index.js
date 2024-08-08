771a0c869ecc5f8ff96a59caf864079af5870996
Ryan Bowman <[email protected]>
2024-08-08 12:50:34 -0400

    Created function to get the computer choice of rock, paper, or scissors and then display it as an image in the center of the main div

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hi")
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Hello")
+    getComputerChoice();
+})
+
+function getComputerChoice() {
+let computerOptions = ['rock', 'paper', 'scissors'];
+let computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+let computerChoiceImgOutput = document.querySelector('#computer-result');
+console.log(computerChoice);
+
+if (computerChoice === 'rock'){
+    computerChoiceImgOutput.innerHTML = "Computer selects: <img src=\"https://img.icons8.com/emoji/48/rock-emoji.png\" width=\"48px\" height=\"48px\">"
+} else if (computerChoice === 'paper'){
+    computerChoiceImgOutput.innerHTML = "Computer Selects: <img src=\"https://img.icons8.com/dusk/48/paper.png\" width=\"48px\" height=\"48px\">"
+} else {
+    computerChoiceImgOutput.innerHTML = "Computer Selects: <img src=\"https://img.icons8.com/color/48/scissors.png\" width=\"48px\" height=\"48px\">"
+}
+};
