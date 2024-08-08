b95f6ae4795676f3b0fdf4db98a0b767b02f35d0
Ryan Bowman <[email protected]>
2024-08-08 16:57:14 -0400

    Created functions 'weaponChoice()' and 'playRound()' to handle each round played and its output to the HTML file. Have done a LOT of maneuvering around with JS to get it so that the user and computer outputs are triggered at the same time.

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Hello")
-    getComputerChoice();
-    getUserChoice();
+    weaponChoice();
 })
 
-function getComputerChoice() {
-let computerOptions = ['rock', 'paper', 'scissors'];
-let computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
-let computerChoiceImgOutput = document.querySelector('#computer-result');
-console.log(computerChoice);
+let computerScore = 0;
+let userScore = 0;
+const weaponOptions = ['rock', 'paper', 'scissors'];
 
-if (computerChoice === 'rock'){
-    computerChoiceImgOutput.innerHTML = "Computer selects: <img src=\"https://img.icons8.com/emoji/48/rock-emoji.png\" width=\"48px\" height=\"48px\">"
-} else if (computerChoice === 'paper'){
-    computerChoiceImgOutput.innerHTML = "Computer Selects: <img src=\"https://img.icons8.com/dusk/48/paper.png\" width=\"48px\" height=\"48px\">"
-} else {
-    computerChoiceImgOutput.innerHTML = "Computer Selects: <img src=\"https://img.icons8.com/color/48/scissors.png\" width=\"48px\" height=\"48px\">"
-}
-};
 
-function getUserChoice(){
-    let userChoiceRock = document.querySelector('#rock-img');
-    userChoiceRock.addEventListener('click', () => {
-        console.log("You selected rock");
+document.querySelector('#rock-img').addEventListener('click', () => {
+        weaponChoice('rock')
     })
-    let userChoicePaper = document.querySelector('#paper-img');
-    userChoicePaper.addEventListener('click', () => {
-        console.log("You selected paper");
+document.querySelector('#paper-img').addEventListener('click', () => {
+        weaponChoice('paper')
     })
-    let userChoiceScissors = document.querySelector('#scissors-img');
-    userChoiceScissors.addEventListener('click', () => {
-        console.log("You selected scissors");
+document.querySelector('#scissors-img').addEventListener('click', () => {
+        weaponChoice('scissors')
     })
+
+
+function weaponChoice(choice){
+    let userChoice = choice
+    let computerChoice = weaponOptions[Math.floor(Math.random()* weaponOptions.length)];
+    console.log(userChoice);
+    console.log(computerChoice);
+    playRound(userChoice, computerChoice);
 }
+
+
+        
+function playRound(userChoice, computerChoice){
+    let resultMessage = document.querySelector('#round-result-message');
+    let computerChoiceImgOutput = document.querySelector('#computer-result');
+    if (userChoice === 'rock' && computerChoice === 'rock') {
+        computerChoiceImgOutput.innerHTML = "Computer selected: <img src=\"https://img.icons8.com/emoji/48/rock-emoji.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "That is a tie:/"
+    } else if (userChoice === 'paper' && computerChoice === 'paper') {
+        computerChoiceImgOutput.innerHTML = "Computer Selected: <img src=\"https://img.icons8.com/dusk/48/paper.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "That is a tie:/"
+    } else if (userChoice === 'scissors' && computerChoice === 'scissors'){
+        computerChoiceImgOutput.innerHTML = "Computer Selected: <img src=\"https://img.icons8.com/color/48/scissors.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "That is a tie:/"
+    } else if(userChoice === 'rock' && computerChoice === 'scissors'){
+        computerChoiceImgOutput.innerHTML = "Computer Selected: <img src=\"https://img.icons8.com/color/48/scissors.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "You win!!"
+    } else if (userChoice === 'paper' && computerChoice === 'rock'){
+        computerChoiceImgOutput.innerHTML = "Computer selected: <img src=\"https://img.icons8.com/emoji/48/rock-emoji.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "You win!!"
+    } else if (userChoice === 'scissors' && computerChoice === 'paper'){
+        computerChoiceImgOutput.innerHTML = "Computer Selected: <img src=\"https://img.icons8.com/dusk/48/paper.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "You win!!"
+    } else if(userChoice === 'rock' && computerChoice === 'paper'){
+        computerChoiceImgOutput.innerHTML = "Computer Selected: <img src=\"https://img.icons8.com/dusk/48/paper.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "Computer wins:("
+    } else if (userChoice === 'paper' && computerChoice === 'scissors'){
+        computerChoiceImgOutput.innerHTML = "Computer Selected: <img src=\"https://img.icons8.com/color/48/scissors.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "Computer wins:("
+    } else if (userChoice === 'scissors' && computerChoice === 'rock'){
+        computerChoiceImgOutput.innerHTML = "Computer selected: <img src=\"https://img.icons8.com/emoji/48/rock-emoji.png\" width=\"48px\" height=\"48px\">"
+        resultMessage.innerHTML = "Computer wins:("
+    }
+    }
+
+
+
+
